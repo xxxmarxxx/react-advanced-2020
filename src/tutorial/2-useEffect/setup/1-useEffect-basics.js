61b80ca5e61e3013bb3f0e73e04e58dd45a831be
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-21 14:36:43 +0100

    update

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -2,8 +2,23 @@ import React, { useState, useEffect } from 'react';
 // by default runs after every re-render
 // cleanup function
 // second parameter
+// domyślnie uruchamia się po każdym re-render
+// funkcja oczyszczania
+// drugi parametr
+
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+const [value, setValue] = useState(0);
+  useEffect(()=>{
+console.log('call useEffect');
+document.title = `to cwiczenia(${value})`
+  })
+  console.log('render component');
+  return <>
+  <h1>{value}</h1>
+  <button className='btn' onClick={()=> setValue(value +1)}>
+    click me
+  </button>
+  </>
 };
 
 export default UseEffectBasics;
